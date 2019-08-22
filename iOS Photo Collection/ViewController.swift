32de85292acda8824d8ea826a2c6b779a40748ca
Andrew Ruiz <nukedoom@gmail.com>
2019-08-22 11:39:54 -0700

    Initial Commit

---
+++ iOS Photo Collection/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOS Photo Collection
+//
+//  Created by Andrew Ruiz on 8/22/19.
+//  Copyright © 2019 Andrew Ruiz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
